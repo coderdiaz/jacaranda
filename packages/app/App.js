ad66677b5d86001f342fa0fc188152b3fad7837d
Javier Diaz <coderdiaz@gmail.com>
2025-02-25 00:17:14 -0600

    chore(example): added a new expo app with a button example

---
+++ packages/app/App.js
@@ -0,0 +1,49 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { styles } from './lib/tokens';
+
+export const Button = (props) => {
+  return (<TouchableOpacity style={buttonStyles({ intent: props.intent, size: props.size })}>
+    <Text>Button</Text>
+  </TouchableOpacity>)
+}
+
+const buttonStyles = styles({
+  base: { borderRadius: '$spacing.md' },
+  variants: {
+    intent: {
+      primary: {
+        backgroundColor: '$colors.primary50',
+      },
+      secondary: {
+        backgroundColor: '$colors.secondary50',
+      }
+    },
+    size: {
+      md: {
+        paddingHorizontal: '$spacing.md',
+        paddingVertical: '$spacing.sm',
+      }
+    }
+  },
+  defaultVariants: {
+    intent: 'primary',
+    size: 'md'
+  }
+})
+
+export default function App() {
+  return (
+    <View style={sty.container}>
+      <Button intent="primary" />
+    </View>
+  );
+}
+
+const sty = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ packages/app/lib/tokens.ts
@@ -0,0 +1,31 @@
+import { createTokens } from "jacaranda";
+
+export const { styles } = createTokens({
+  colors: {
+    primary50: '#ecfeff',
+    primary100: '#cffafe',
+    primary200: '#a5f3fc',
+    primary300: '#67e8f9',
+    primary400: '#22d3ee',
+    primary500: '#06b6d4',
+    primary600: '#0e93d1',
+    primary700: '#1570ad',
+    // Secondary
+    secondary50: '#ecfdf5',
+    secondary100: '#d9f9eb',
+    secondary200: '#bbfde8',
+    secondary300: '#86f9d9',
+    secondary400: '#0d9488',
+    secondary500: '#027a7b',
+    secondary600: '#016464',
+    secondary700: '#014747',
+  },
+  spacing: {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 32,
+    xxl: 40,
+  },
+})
